9b1bf8c0a9be43ba0be536a74fbadf2aa48d5f96
damaneks <damaneks@interia.pl>
2021-05-02 19:35:13 +0200

    Detect from image to local

--- app.py
+++ app.py
@@ -1,11 +1,29 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
+from werkzeug.utils import secure_filename
 
+from tft_detection import tft_detector
+import os
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/detect', methods=['GET', 'POST'])
+def upload():
+    if request.method == 'POST':
+        f = request.files['file']
+        
+        basepath = os.path.dirname(__file__)
+        file_path = os.path.join(basepath, 'uploads', secure_filename(f.filename))
+        f.save(file_path)
+
+        detection = tft_detector(file_path)
+
+        return redirect('/')
+    else:   
+        return None
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ tft_detection.py
@@ -0,0 +1,65 @@
+import tensorflow as tf
+physical_devices = tf.config.experimental.list_physical_devices('GPU')
+if len(physical_devices) > 0:
+    tf.config.experimental.set_memory_growth(physical_devices[0], True)
+from tensorflow.python.saved_model import tag_constants
+from absl import app, flags, logging
+from absl.flags import FLAGS
+from tensorflow.compat.v1 import ConfigProto
+from tensorflow.compat.v1 import InteractiveSession
+import cv2
+import core.utils as utils
+import numpy as np
+from PIL import Image
+
+input_size = 416
+weights="./checkpoints/starting-416"
+iou = 0.45
+score = 0.25
+output = './detections/'
+
+def tft_detector(image_path):
+    config = ConfigProto()
+    config.gpu_options.allow_growth = True
+    session = InteractiveSession(config=config)
+    STRIDES, ANCHORS, NUM_CLASS, XYSCALE = utils.load_config(FLAGS)
+    
+    
+    
+    original_image = cv2.imread(image_path)
+    original_image = cv2.cvtColor(original_image, cv2.COLOR_BGR2RGB)
+
+    image_data = cv2.resize(original_image, (input_size, input_size))
+    image_data = image_data / 255.
+
+    images_data = []
+    for i in range(1):
+        images_data.append(image_data)
+    images_data = np.asarray(images_data).astype(np.float32)
+    
+    saved_model_loaded = tf.saved_model.load(weights, tags=[tag_constants.SERVING])
+    infer = saved_model_loaded.signatures['serving_default']
+    batch_data = tf.constant(images_data)
+    pred_bbox = infer(batch_data)
+    for key, value in pred_bbox.items():
+        boxes = value[:, :, 0:4]
+        pred_conf = value[:, :, 4:]
+    
+    boxes, scores, classes, valid_detections = tf.image.combined_non_max_suppression(
+        boxes=tf.reshape(boxes, (tf.shape(boxes)[0], -1, 1, 4)),
+        scores=tf.reshape(
+            pred_conf, (tf.shape(pred_conf)[0], -1, tf.shape(pred_conf)[-1])),
+        max_output_size_per_class=50,
+        max_total_size=50,
+        iou_threshold=iou,
+        score_threshold=score
+    )
+
+    pred_bbox = [boxes.numpy(), scores.numpy(), classes.numpy(), valid_detections.numpy()]
+    image = utils.draw_bbox(original_image, pred_bbox)
+    image = Image.fromarray(image.astype(np.uint8))
+    image = cv2.cvtColor(np.array(image), cv2.COLOR_BGR2RGB)
+
+    cv2.imwrite(output + 'Detection1.jpg', image)
+    return image
+
